9c11d891516d01c1ed676bdd67798ba582278f4e
DavG25 <31524206+DavG25@users.noreply.github.com>
2019-05-30 14:40:00 +0200

    Add files via upload

--- extension/core.js
+++ extension/core.js
@@ -1,12 +1,28 @@
+//	core.js
+//		This file will handle the webRequest
+
+
+//	Define the User-Agent header to use on working sites
+var UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
+
+
+//	Hook event to webRequest before the browser headers are sent
 chrome.webRequest.onBeforeSendHeaders.addListener(
+	//	Get request details
     function(details) {
         for (var i = 0; i < details.requestHeaders.length; ++i) {
+			//	Check if current detail header is User-Agent
             if (details.requestHeaders[i].name === 'User-Agent') {
-                details.requestHeaders[i].value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36";
+				//	Change User-Agent header
+                details.requestHeaders[i].value = UA;
                 break;
             }
         }
+		//	Return headers
         return {requestHeaders: details.requestHeaders};
-    }, 
-{urls: ['*://*.youtube.com/*']}, ['blocking', 'requestHeaders']
+    },
+	//	Working sites list
+	{urls: ['*://*.youtube.com/*']},
+	//	Type of event
+	['blocking', 'requestHeaders']
 );
